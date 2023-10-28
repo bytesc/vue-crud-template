10b7baaebf99f2a2bd326ce0b9e823298f4cbb1f
rcy <21010078@mail.ecust.edu.cn>
2023-10-28 11:58:47 +0800

    build

--- vue-crud/vite.config.js
+++ vue-crud/vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    port : 8080 //指定部署端口号
+  },
+  base: "./" //打包相对路径
 })
