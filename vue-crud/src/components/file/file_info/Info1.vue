499eac36dbd31af9fcdbd059e13c3d497170cc5f
rcy <21010078@mail.ecust.edu.cn>
2023-10-31 20:36:51 +0800

    route control

--- vue-crud/src/components/file/file_info/Info1.vue
+++ vue-crud/src/components/file/file_info/Info1.vue
@@ -1,9 +1,10 @@
 <template>
   <div style="text-align: center" v-show="!hasChildRoute">
 <!--    只在没有子路由的时候显示-->
-    这是信息一  <a :href=url>详情</a>
+    这是信息一  <a :href=url_detail1>详情</a>
   </div>
-  <router-view></router-view>
+<!--  向下一级传递本级url来实现返回功能-->
+  <router-view :urlNow="urlNow"></router-view>
 </template>
 
 <script setup>
@@ -11,11 +12,12 @@
 import {useRoute} from "vue-router";
 import {ref, watch} from "vue";
 
-let url = window.location.href + "/detail1"
+let url_detail1 = window.location.href + "/detail1"
+let urlNow = window.location.href
 let route = useRoute()
 let hasChildRoute = ref(false)
 
-//通过路由数量判断是否有子路由
+//通过路由数量route.matched.length判断是否有子路由
 watch(() => route.matched, () => {
   hasChildRoute.value = route.matched.length > 2
   console.log(route.matched.length)
--- vue-crud/src/components/file/file_info/detail/Detail1.vue
+++ vue-crud/src/components/file/file_info/detail/Detail1.vue
@@ -1,10 +1,14 @@
 <template>
   <div style="text-align: center">
-    这是详情一
+    <a :href=urlNow>返回</a>这是详情一
   </div>
 </template>
 
 <script setup>
+//接收上一级url来实现返回功能
+const props = defineProps({
+  urlNow:String,
+})
 
 </script>
 
