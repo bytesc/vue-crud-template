3be744c34f7d54a8c8f5b9d38f8d85e9c96036ad
rcy <21010078@mail.ecust.edu.cn>
2023-11-07 16:33:58 +0800

    gorm

--- go-crud/main.go
+++ go-crud/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 	"time"
 )
 
@@ -12,7 +13,20 @@ func main() {
 	//链接数据库
 	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
 	dsn := "root:123456@tcp(127.0.0.1:3306)/crud-list?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			//TablePrefix: "shop_",
+			SingularTable: true, //表名单数
+		},
+	})
+	//db, err := gorm.Open(mysql.New(mysql.Config{
+	//	DSN:                       "root:123456@tcp(127.0.0.1:3306)/crud-list?charset=utf8mb4&parseTime=True&loc=Local", // DSN data source name
+	//	DefaultStringSize:         256,                                                                                  // string 类型字段的默认长度
+	//	DisableDatetimePrecision:  true,                                                                                 // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+	//	DontSupportRenameIndex:    true,                                                                                 // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+	//	DontSupportRenameColumn:   true,                                                                                 // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+	//	SkipInitializeWithVersion: false,                                                                                // 根据当前 MySQL 版本自动配置
+	//}), &gorm.Config{})
 	fmt.Println(db, err)
 
 	sqlDB, err := db.DB()
@@ -26,14 +40,20 @@ func main() {
 
 	//gorm结构体
 	type CrudList struct {
-		Id       string
-		Name     string
-		Level    string
-		Email    string
-		Phone    string
-		Birthday string
+		gorm.Model // 引入模板结构体
+		//ID        uint `gorm:"primarykey"`
+		//CreatedAt time.Time
+		//UpdatedAt time.Time
+		//DeletedAt DeletedAt `gorm:"index"`
+		Name     string `gorm:"type: varchar(20); not null" binding:"required" json:"name" `
+		Level    string `gorm:"type: varchar(20);" json:"level"`
+		Email    string `gorm:"type: varchar(50);" json:"email"`
+		Phone    string `gorm:"type: varchar(20);" json:"phone"`
+		Birthday string `gorm:"type: varchar(20);" json:"birthday"`
+		Address  string `gorm:"type: varchar(200);" json:"address"`
 		// 变量名要大写，才public，可以被gorm获取解析
 	}
+
 	// 数据库迁移
 	err = db.AutoMigrate(&CrudList{})
 
